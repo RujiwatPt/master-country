57a4f9e18de8892d7cb991f63fcf8ae4c471be57
Rujiwat Pithaksiripan <howlinglight@Rujiwats-MacBook-Pro.local>
2023-09-23 08:48:27 +0700

    swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -8,6 +9,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRoot(
       'mongodb+srv://rujiwat:masterCountry@mastercountry.x4lijzu.mongodb.net/?retryWrites=true&w=majority',
     ),
+    SwaggerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
